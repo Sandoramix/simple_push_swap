89f5371c84c426f90a06b1c758a7e08b408e1f9c
Oleksandr Dudniak <odudniak@student.42firenze.it>
2025-03-01 16:44:17 +0100

    removed unnecessary logic

--- ps_parsing.c
+++ ps_parsing.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-t_list	*populate(char **av, bool should_free)
+t_list	*populate(char **av)
 {
 	t_list	*res;
 	int		i;
@@ -25,33 +25,18 @@ t_list	*populate(char **av, bool should_free)
 		n = ft_atoi(av[i]);
 		if (!is_just_a_number(av[i]) || lst_findval(res, n) != NULL
 			|| !lst_addback(&res, n))
-		{
-			if (should_free)
-				mtx_free(&av);
 			return (lst_free(&res), write(2, "Error\n", 7), exit(1), NULL);
-		}
 	}
 	return (res);
 }
 
 t_list	*parse(int ac, char **av)
 {
-	bool	should_free;
 	t_list	*res;
 
-	should_free = false;
 	if (ac < 2)
 		return (NULL);
 	av = &av[1];
-	if (ac == 2)
-	{
-		should_free = true;
-		av = ft_split(*av);
-		if (!av || !av[0])
-			return (write(2, "Error\n", 7), mtx_free(&av), exit(1), NULL);
-	}
-	res = populate(av, should_free);
-	if (should_free)
-		mtx_free(&av);
+	res = populate(av);
 	return (res);
 }
